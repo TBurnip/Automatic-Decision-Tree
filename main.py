a6185478b141723fc3a322f1cb47d1a768f0dd75
TBurnip <tb@tburnip.co.uk>
2019-07-04 13:39:31 +0100

    Merged Templates into one template

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import pystache,json
 
-template = {"category":"","action":"","advice":"","question":""}
 
-for t in template:
-    with open("template_"+ t +".html") as f:
-        template[t] = "".join(f)
+with open("template.html") as f:
+    template = "".join(f)
 
 with open("data.json") as f:
     data = json.load(f)
 
 for page in data["Page"]:
-    if page["type"] in template:
-        np = pystache.render(template[page["type"]],page)
-        with open("static/"+page["filename"]+".html","w") as f:
-            f.write(np)
+    np = pystache.render(template,page)
+    with open("static/"+page["filename"]+".html","w") as f:
+        f.write(np)
