2c4b92d99f942f71089cfe5c2735ba732f6d2ac0
TBurnip <tb@tburnip.co.uk>
2019-07-04 13:16:53 +0100

    First commit

---
+++ serve.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+var port string
+var static string
+
+func init() {
+	port = os.Getenv("port")
+	static = os.Getenv("static")
+}
+
+func main() {
+	println(port)
+	http.Handle("/", http.FileServer(http.Dir(static)))
+	http.ListenAndServe(":"+port, nil)
+}
